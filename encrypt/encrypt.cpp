c155c25381bbd42301c32e2f1fef8bd8698e67b5
nemu-ism <vanilla_chip_s@yahoo.co.jp>
2018-04-12 05:34:53 +0900

    和差算でお茶濁し＆n回暗号化

--- encrypt/encrypt.cpp
+++ encrypt/encrypt.cpp
@@ -5,6 +5,7 @@
 
 #define key 412
 #define SIZE 1024
+#define n 2018
 
 void encrypt(char* file) {
 	char str[SIZE];
@@ -15,14 +16,14 @@ void encrypt(char* file) {
 	if (fopen_s(&rf, file, "rb") != 0) {
 		std::cout << "ファイルが見つかりません" << std::endl;
 		exit(0);
-	}
-	else {
+	} else {
 		error = fopen_s(&wf, "encrypted.jpg", "wb");
 		while (1) {
 			int l = fread(str, 1, SIZE, rf);
 			if (l < 1) break;
 			for (int i = 0; i < l; i++) {
-				putc(str[i] ^ key, wf);
+				for (int j = 0; j <= n; j++) str[i] = (str[i] ^ key) + n;
+				putc(str[i], wf);
 			}
 		}
 	}
@@ -33,20 +34,21 @@ void encrypt(char* file) {
 
 void decrypt() {
 	char str[SIZE];
+	int sft;
 	FILE *rf;
 	FILE *wf;
 	errno_t error;
 	if (fopen_s(&rf, "encrypted.jpg", "rb") != 0) {
 		std::cout << "ファイルが見つかりません" << std::endl;
 		exit(0);
-	}
-	else {
+	} else {
 		error = fopen_s(&wf, "decrypted.jpg", "wb");
 		while (1) {
 			int l = fread(str, 1, SIZE, rf);
 			if (l < 1) break;
 			for (int i = 0; i < l; i++) {
-				putc(str[i] ^ key, wf);
+				for (int j = 0; j <= n; j++) str[i] = (str[i] - n) ^ key;
+				putc(str[i], wf);
 			}
 		}
 	}
