00add0a21705e5301b7a4c912880b208333f9ba1
nemu-ism <vanilla_chip_s@yahoo.co.jp>
2018-04-12 09:07:15 +0900

    add decrypt code

--- encrypt/encrypt.cpp
+++ encrypt/encrypt.cpp
@@ -14,7 +14,7 @@ void encrypt(char* read, char* write) {
 	errno_t error;
 
 	if (fopen_s(&rf, read, "rb") != 0 || write == NULL) {
-		std::cout << "ƒtƒ@ƒCƒ‹‚ªŒ©‚Â‚©‚è‚Ü‚¹‚ñ" << std::endl;
+		std::cout << "ãƒ•ã‚¡ã‚¤ãƒ«ãŒè¦‹ã¤ã‹ã‚Šã¾ã›ã‚“" << std::endl;
 		exit(0);
 	} else {
 		error = fopen_s(&wf, write, "wb");
@@ -32,8 +32,36 @@ void encrypt(char* read, char* write) {
 	fclose(wf);
 }
 
+//decrypt.exeç”¨
+void decrypt(char* read, char* write) {
+	char str[SIZE];
+	FILE *rf;
+	FILE *wf;
+	errno_t error;
+
+	if (fopen_s(&rf, read, "rb") != 0 || write == NULL) {
+		std::cout << "ãƒ•ã‚¡ã‚¤ãƒ«ãŒè¦‹ã¤ã‹ã‚Šã¾ã›ã‚“" << std::endl;
+		exit(0);
+	}
+	else {
+		error = fopen_s(&wf, write, "wb");
+		while (1) {
+			int l = fread(str, 1, SIZE, rf);
+			if (l < 1) break;
+			for (int i = 0; i < l; i++) {
+				for (int j = 0; j <= n; j++) str[i] = (str[i] - n) ^ key;
+				fwrite(&str[i], sizeof(str[i]), 1, wf);
+			}
+		}
+	}
+
+	fclose(rf);
+	fclose(wf);
+}
+
 int main(int argc, char* argv[])
 {
 	encrypt(argv[1], argv[2]);
+	//decrypt(argv[1], argv[2]);
     return 0;
 }
